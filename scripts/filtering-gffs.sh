954e68b55685ce9bd60cf672f7ff44986b921754
Scott Daniel <scottdaniel25@gmail.com>
2018-05-15 10:37:47 -0700

    filter gff script working

--- scripts/filtering-gffs.sh
+++ scripts/filtering-gffs.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
 #to be used to data munge those gff's
-#put in the genome directory where your gff's are
 
-#get rid of empty lines
-egrep -v '^$' all-PATRIC.gff > allPATRIC.gff.temp
-#get rid of comments
-egrep -v '^#' allPATRIC.gff.temp > temp2
-#only get CDS
-egrep "\tCDS\t" temp2 > all-PATRIC-CDS.gff
-#get the rRNA for bowtie2 (or other aligners) that support filtering by rRNA seq
-egrep "\trRNA\t" temp2 > all-PATRIC-rRNA.gff
+[[ $# -eq 0 ]] && echo "Usage: $0 [gff name]" && exit 1
+
+DIR=$(dirname $1)
+BASE=$(basename $1 .gff)
+
+echo get rid of empty lines
+egrep -v '^$' $1 > $DIR/$BASE.temp
+echo get rid of comments
+egrep -v '^#' $DIR/$BASE.temp > $DIR/$BASE.temp2
+echo only get CDS
+egrep "\tCDS\t" $DIR/$BASE.temp2 > $DIR/$BASE-CDS.gff
+echo "get the rRNA for cuffdiff (or other progs) that support filtering by rRNA seq"
+egrep "\trRNA\t" $DIR/$BASE.temp2 > $DIR/$BASE-rRNA.gff 
+
+echo removing temp files
+rm $DIR/$BASE.temp
+rm $DIR/$BASE.temp2
