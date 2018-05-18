3bbbba8e04bd870c53f81052b5b362577d73eb97
Scott G. Daniel <scottdaniel25@gmail.com>
2018-05-18 16:21:08 +0000

    filtering for htseq-count now

--- scripts/filtering-gffs.sh
+++ scripts/filtering-gffs.sh
@@ -13,7 +13,9 @@ echo get rid of empty lines
 egrep -v '^$' $1 > "$DIR/$BASE.temp"
 echo get rid of comments
 egrep -v '^#' $DIR/$BASE.temp > "$DIR/$BASE.temp2"
-file "$DIR/$BASE.temp2"
+echo get rid of 'accn|' for htseq-count
+sed -i 's/^accn|//' "$DIR/$BASE.temp2"
+#file "$DIR/$BASE.temp2"
 echo only get CDS
 grep -P "\tCDS\t" "$DIR/$BASE.temp2" > "$DIR/$BASE-CDS.gff"
 echo "get the rRNA for cuffdiff (or other progs) that support filtering by rRNA seq"
