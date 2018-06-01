1687b51e1dc16069ee71a70c2c6e65d6d042a51c
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-01 00:10:22 +0000

    trying to get this image built

--- scripts/install.r
+++ scripts/install.r
@@ -1,7 +1,15 @@
 #!/usr/bin/env Rscript
 
 #script to install the most-up-to-date R-packages
+myPaths <- .libPaths()
+
+myPaths <- c(‘/media/miniconda/lib/R/library’, myPaths)
+
+.libPaths(myPaths)  # add new path
 
 library(devtools)
+
 if(getOption("unzip") == "") options(unzip = 'internal')
-install_github("PF2-pasteur-fr/SARTools")
+#install.packages("stringi", configure.args="--disable-pkg-config")
+install_github("PF2-pasteur-fr/SARTools", quick = TRUE, dependencies = FALSE,
+               upgrade_dependencies = FALSE)
