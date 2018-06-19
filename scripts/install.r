577601e75264649faee7cb1058d1ae830bebc427
Scott G. Daniel <scottdaniel25@gmail.com>
2018-06-19 23:04:27 +0000

    no longer needed

--- scripts/install.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-#install SARTools from github
-library(devtools)
-
-if(getOption("unzip") == "") options(unzip = 'internal')
-#install.packages("stringi", configure.args="--disable-pkg-config")
-install_github("PF2-pasteur-fr/SARTools", quick = TRUE, dependencies = FALSE,
-               upgrade_dependencies = FALSE)
